e1bd066c86cdab52c3f02775ad00d9f2d58e0744
Angel Kurten <angelkurten@hotmail.com>
2015-10-21 20:32:18 -0500

    Initial Comiit

---
+++ Entities/Module.php
@@ -0,0 +1,19 @@
+<?php namespace Modules\Course\Entities;
+   
+use Illuminate\Database\Eloquent\Model;
+
+class Module extends Model {
+
+    protected $fillable = [];
+
+
+    public function lessons()
+    {
+        return $this->hasMany(Lesson::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+}
---
+++ Resources/lang/es/show.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+        'messages' => [
+                    'sharer'        => 'Recomiendo el curso :name de PVA via @academyphp',
+                    'welcome'       => 'Bienvenido a ',
+                    'modules'       => 'Modulos',
+                    'teacher'       => 'Profesor'
+        ],
+
+        'btn'      =>[
+            'module'        => 'Modulos',
+            'favorite'      => 'Favorito',
+            'video'         => 'Es video',
+            'note'          => 'Es nota',
+            'view'          => 'Ya la viste'
+        ],
+];
