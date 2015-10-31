28162553f06b472636507e1e22f2ba1d89652fad
Angel Kurten <angelkurten@hotmail.com>
2015-10-30 19:14:54 -0500

    Fix route

--- Http/routes.php
+++ Http/routes.php
@@ -1,28 +1,5 @@
 <?php
 
-/*Route::group(['prefix' => 'learning', 'namespace' => 'Modules\Course\Http\Controllers\Learning'], function(){
-
-    Route::get('/', ['as'=>'dashboard.learning', 'uses'=>'CourseController@index']);
-
-    Route::resource('course', 'CourseController',
-        [
-            'except' => ['create', 'store', 'update', 'destroy']
-        ]
-    );
-
-    Route::resource('lesson', 'LessonController',
-        [
-            'except' => ['index','create', 'store', 'update', 'destroy']
-        ]
-    );
-
-    Route::resource('comment', 'CommentController',
-        [
-            'except' => ['index','create', 'update']
-        ]
-    );
-});*/
-
 Route::group(['prefix' => 'admin', 'namespace' => 'Modules\Course\Http\Controllers\Admin'], function(){
 
     Route::resource('course', 'CourseController',
@@ -30,3 +7,12 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Modules\Course\Http\Controlle
     );
 
 });
+
+Route::group(['prefix' => 'learning', 'namespace' => 'Modules\Course\Http\Controllers\learning'], function(){
+
+    Route::resource('course', 'CourseController',
+        ['except' => ['create', 'store', 'update', 'destroy']]
+    );
+
+});
+
