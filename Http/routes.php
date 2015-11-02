a92c34eb85f760220214dda0643f7bb3d1286b9c
Angel Kurten <angelkurten@hotmail.com>
2015-11-02 13:04:49 -0500

    Middleware auth for learning routes

--- Http/routes.php
+++ Http/routes.php
@@ -8,7 +8,7 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Modules\Course\Http\Controlle
 
 });
 
-Route::group(['prefix' => 'learning', 'namespace' => 'Modules\Course\Http\Controllers\learning'], function(){
+Route::group(['prefix' => 'learning', 'namespace' => 'Modules\Course\Http\Controllers\learning', 'middleware' => 'auth'], function(){
 
     Route::resource('course', 'CourseController',
         ['except' => ['create', 'store', 'update', 'destroy']]
